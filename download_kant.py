c83eacab40b93836a5ae21f0659059f975f74b8a
Mitchell Reynolds <mitchell.s.reynolds@gmail.com>
2018-08-30 19:11:30 -0700

    download_kant puts txt files into texts but does not commit those to GH

--- download_kant.py
+++ download_kant.py
@@ -1,3 +1,5 @@
+import os.path
+
 from gutenberg.acquire import load_etext
 from gutenberg.cleanup import strip_headers
 
@@ -5,3 +7,15 @@ print("Downloading Morals")
 morals = strip_headers(load_etext(5682)).strip()
 print("Downloading Ethics")
 ethics = strip_headers(load_etext(5684)).strip()
+
+
+def save(filename, contents):
+
+    name_and_folder = os.path.join("./texts/", filename)
+
+    fh = open(name_and_folder, 'w')
+    fh.write(contents)
+    fh.close()
+
+save('morals.txt', morals)
+save('ethics.txt', ethics)
